b979b35b7f99983961df1bed714deda5b27c4c88
Redonearth <wona23@gmail.com>
2020-07-12 19:50:33 +0900

    Updates

---
+++ precache-manifest.fc01305913dea087cd1051c90689dd82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bca7d33b0ac62749333b30d55eab365",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5533ab1fb083a8563806",
+    "url": "/movie_app/static/css/main.904f01ca.chunk.css"
+  },
+  {
+    "revision": "6137fd49fc0832987548",
+    "url": "/movie_app/static/js/2.7342ae0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.7342ae0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5533ab1fb083a8563806",
+    "url": "/movie_app/static/js/main.cabf618a.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.9f94781911a4bf2c9d058ee1c473df33.js"
+  "/movie_app/precache-manifest.fc01305913dea087cd1051c90689dd82.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movie_app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
