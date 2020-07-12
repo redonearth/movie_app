caf2221fb37d5bf4ee256ea193e40d87751961f5
Redonearth <wona23@gmail.com>
2020-07-12 19:13:34 +0900

    Fix the malfunction

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
-
-  state = {}
+  state = {};
 
   componentDidMount() {
     this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map(movie => {
-      return <Movie
-        title={movie.title_english}
-        poster={movie.medium_cover_image}
-        genres={movie.genres}
-        synopsis={movie.synopsis}
-        key={movie.id}
-      />
-    })
-    return movies
-  }
+    const movies = this.state.movies.map((movie) => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+          key={movie.id}
+        />
+      );
+    });
+    return movies;
+  };
 
   _getMovies = async () => {
-    const movies = await this._callApi()
+    const movies = await this._callApi();
     this.setState({
-      movies
-    })
-  }
+      movies,
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-      .then(potato => potato.json())
-      .then(json => json.data.movies)
-      .catch(err => console.log(err))
-  }
+    return fetch(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then((potato) => potato.json())
+      .then((json) => json.data.movies)
+      .catch((err) => console.log(err));
+  };
 
   render() {
     const { movies } = this.state;
     return (
       <div className={movies ? "App" : "App--loading"}>
-        {movies ? this._renderMovies() : 'Loading'}
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import LinesEllipsis from 'react-lines-ellipsis';
-import './Movie.css';
+import React from "react";
+import PropTypes from "prop-types";
+import LinesEllipsis from "react-lines-ellipsis";
+import "./Movie.css";
 
-function Movie({title, poster, genres, synopsis}) {
+function Movie({ title, poster, genres, synopsis }) {
   return (
     <div className="Movie">
       <div className="Movie__Column">
@@ -12,48 +12,46 @@ function Movie({title, poster, genres, synopsis}) {
       <div className="Movie__Column">
         <h1>{title}</h1>
         <div className="Movie__Genres">
-          {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+          {genres.map((genre, index) => (
+            <MovieGenre genre={genre} key={index} />
+          ))}
         </div>
         <div className="Movie__Synopsis">
           <LinesEllipsis
             text={synopsis}
-            maxLine='3'
-            ellipsis='...'
+            maxLine="3"
+            ellipsis="..."
             trimRight
-            basedOn='letters'
+            basedOn="letters"
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-function MoviePoster({poster, alt}) {
-  return (
-    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
-  )
+function MoviePoster({ poster, alt }) {
+  return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
 }
 
-function MovieGenre({genre}) {
-  return (
-    <span className="Movie__Genre">{genre}</span>
-  )
+function MovieGenre({ genre }) {
+  return <span className="Movie__Genre">{genre}</span>;
 }
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   genres: PropTypes.array.isRequired,
-  synopsis: PropTypes.string.isRequired
-}
+  synopsis: PropTypes.string.isRequired,
+};
 
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
-}
+  alt: PropTypes.string.isRequired,
+};
 
 MovieGenre.propTypes = {
-  genre: PropTypes.string.isRequired
-}
+  genre: PropTypes.string.isRequired,
+};
 
 export default Movie;
